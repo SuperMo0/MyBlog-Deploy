863b94144d628d3c857e265c28b7c6e16498f5fb
super <97206982+SuperMo0@users.noreply.github.com>
2025-12-25 12:00:00 +0200

    add footer component

---
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BsGithub } from 'react-icons/bs'
+
+export default function Footer() {
+    return (
+        <footer className="py-8 flex justify-center items-center gap-3 text-(--text-secondary) text-sm border-t border-(--border-color)">
+            © {new Date().getFullYear()} MyBlog <a href="https://github.com/SuperMo0"><BsGithub size={25} /> </a>
+        </footer>
+    )
+}
